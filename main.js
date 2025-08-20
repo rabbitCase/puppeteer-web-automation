36df7de14ca1cbde5416e7f5960af4ec717331d4
rabbitCase <gnair3004@gmail.com>
2025-08-20 19:34:59 +0530

    init

---
+++ main.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer');
+
+(async () =>{
+    const browser = await puppeteer.launch({headless : false});
+    const page = await browser.newPage();
+
+    await page.goto('https://amazon.in/');
+    await page.setViewport({width: 1080, height: 1024});
+
+    await page.type('#twotabsearchtextbox', 'electric guitar');
+    const searchButton = '#nav-search-submit-button';
+    await page.click(searchButton);
+
+    const sortButton = '#a-autoid-0-announce';
+    const sortByPrice = '#s-result-sort-select_1';
+    const addToCart = '#a-autoid-3-announce';
+
+    await page.waitForSelector(sortButton);
+    await page.click(sortButton);
+
+    await page.waitForSelector(sortByPrice);
+    await page.click(sortByPrice);
+
+    await new Promise(res => setTimeout(res, 4000));
+    await browser.close();
+})();
